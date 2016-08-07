9db77a0722f5a4d8aa221a432449c34ce224bf5a
TimonYager <ghostship0704@gmail.com>
2016-08-07 19:02:36 +0300

    facebook action fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    current_user = User.find_for_facebook_oauth request.env["omniauth.auth"]
+    if current_user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in_and_redirect dashboard_path, :event => :authentication
+    else
+      flash[:notice] = "authentication error"
+      redirect_to root_path
   end
 
   def vkontakte
