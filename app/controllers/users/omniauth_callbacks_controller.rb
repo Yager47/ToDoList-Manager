cb2f1667303d89d2f743a6ec8822f03252a20cb8
TimonYager <ghostship0704@gmail.com>
2016-08-08 18:03:04 +0300

    facebook authentication finishedd

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,17 +9,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_path
     end
-
-    # @user = User.find_for_facebook_auth(request.env["omniauth.auth"])
-    # if @user.persisted?
-    #   flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-    #   sign_in @user
-    #   redirect_to dashboard_path, :event => :authentication
-    #   # sign_in_and_redirect @user, :event => :authentication
-    # else
-    #   flash[:notice] = "authentication error"
-    #   redirect_to root_path
-    # end
   end
 
   def vkontakte
