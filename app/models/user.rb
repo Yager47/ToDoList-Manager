6fab7bf7908af7e4b0d0563620f53d04ceb3fb3a
TimonYager <ghostship0704@gmail.com>
2016-08-09 12:26:23 +0300

    vkontakte authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,11 +18,19 @@ class User < ActiveRecord::Base
     task_lists.first
   end
 
-  def self.from_omniauth(auth)
+  def self.facebook_from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|  
       user.provider = auth.provider
       user.uid      = auth.uid
       user.email    = auth.info.email
+    end
+  end
+
+  def self.vkontakte_from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|  
+      user.provider = auth.provider
+      user.uid      = auth.uid
+      user.email    = auth.info.domain + '@vk.com'
     end
   end
 
